8ed5c6d0794613b6088baf23bb328f613b9c7993
sharepointcraft <sharepointcraft@gmail.com>
2025-01-21 13:58:28 +0530

    Added the function- HandleFileSelection

--- src/Components/BulkUpload/NewList.tsx
+++ src/Components/BulkUpload/NewList.tsx
@@ -137,193 +137,198 @@ const NewList: React.FC<INewListProps> = ({ context }) => {
     setIsDialogVisible(false);
   };
 
-  const createSharePointList = async (): Promise<boolean> => {
-    if (!listName || !uniqueId) {
-      //alert("Please provide a list name and select a unique ID.");
-      setErrorPopupMessage(
-        "Please provide a list name and select a unique ID."
-      );
-      setIsPopupOpen(true);
-      return false; // Return false to indicate failure
-    }
-
-    try {
-      const requestDigest = await getRequestDigest(); // Fetch digest dynamically
-
-      // Create the list
-      const listPayload = {
-        __metadata: { type: "SP.List" },
-        Title: listName,
-        BaseTemplate: 100, // Custom List
-      };
-
-      const response = await fetch(`${siteUrl}/_api/web/lists`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;odata=verbose",
-          "X-RequestDigest": requestDigest,
-        },
-        body: JSON.stringify(listPayload),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error creating list: ${response.statusText}`);
-      }
-
-      // Add columns to the list
-      for (let i = 0; i < tableHeaders.length; i++) {
-        const fieldType = columnTypes[i];
-        let metadataType = "SP.Field"; // Default metadata type
-        let fieldTypeKind = 2; // Default to Single Line of Text
-        let additionalProperties = {}; // Default additional properties
-
-        switch (fieldType) {
-          case "DateTime":
-            fieldTypeKind = 4; // DateTime Field
-            metadataType = "SP.FieldDateTime"; // Use the correct type for DateTime
-            additionalProperties = {
-              DisplayFormat: 0, // 0 for DateOnly
-            };
-            break;
-          case "Number":
-            fieldTypeKind = 9; // Number Field
-            break;
-          case "Currency":
-            fieldTypeKind = 10; // Currency Field
-            break;
-          case "Single line of text":
-          default:
-            fieldTypeKind = 2; // Single Line of Text
-        }
-        await fetch(
-          `${siteUrl}/_api/web/lists/getbytitle('${listName}')/fields`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json;odata=verbose",
-              "X-RequestDigest": requestDigest,
-            },
-            body: JSON.stringify({
-              __metadata: { type: metadataType },
-              Title: tableHeaders[i],
-              FieldTypeKind: fieldTypeKind,
-              ...additionalProperties,
-            }),
-          }
-        );
-
-        // Add the column to the default view
-        await fetch(
-          `${siteUrl}/_api/web/lists/getbytitle('${listName}')/defaultview/viewfields/addviewfield('${tableHeaders[i]}')`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json;odata=verbose",
-              "X-RequestDigest": requestDigest,
-            },
-          }
-        );
-      }
-
-      //alert("List created successfully!");
-      return true; // Return true to indicate success
-    } catch (error) {
-      console.error(error);
-      //alert("Error creating the SharePoint list or adding data.");
-      return false; // Return false if an error occurred
-    }
+  const handleFileSelection = (selectedFile: File, record: string) => {
+    console.log(`Selected file:`, selectedFile);
+    setRecordFiles((prev) => ({ ...prev, [record]: selectedFile })); // Store file for each record
   };
 
-  const createDocumentLibrary = async (): Promise<boolean> => {
-    try {
-      const requestDigest = await getRequestDigest(); // Fetch digest dynamically
+  // const createSharePointList = async (siteUrl: string, listName: string, tableHeaders: string[], columnTypes: string[], setErrorPopupMessage: React.Dispatch<React.SetStateAction<string>>, setIsPopupOpen: React.Dispatch<React.SetStateAction<boolean>>, getRequestDigest: () => Promise<string>): Promise<boolean> => {
+  //   if (!listName || !uniqueId) {
+  //     //alert("Please provide a list name and select a unique ID.");
+  //     setErrorPopupMessage(
+  //       "Please provide a list name and select a unique ID."
+  //     );
+  //     setIsPopupOpen(true);
+  //     return false; // Return false to indicate failure
+  //   }
 
-      const libraryPayload = {
-        __metadata: { type: "SP.List" },
-        Title: `${listName}_Documents`,
-        BaseTemplate: 101, // Document Library
-      };
+  //   try {
+  //     const requestDigest = await getRequestDigest(); // Fetch digest dynamically
 
-      const response = await fetch(`${siteUrl}/_api/web/lists`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;odata=verbose",
-          "X-RequestDigest": requestDigest,
-        },
-        body: JSON.stringify(libraryPayload),
-      });
+  //     // Create the list
+  //     const listPayload = {
+  //       __metadata: { type: "SP.List" },
+  //       Title: listName,
+  //       BaseTemplate: 100, // Custom List
+  //     };
 
-      if (!response.ok) {
-        throw new Error(`Error creating library: ${response.statusText}`);
-      }
+  //     const response = await fetch(`${siteUrl}/_api/web/lists`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json;odata=verbose",
+  //         "X-RequestDigest": requestDigest,
+  //       },
+  //       body: JSON.stringify(listPayload),
+  //     });
 
-      //alert("Document library created successfully!");
-      return true;
-    } catch (error) {
-      console.error(error);
-      //alert("Error creating the document library.");
-      return false;
-    }
-  };
+  //     if (!response.ok) {
+  //       throw new Error(`Error creating list: ${response.statusText}`);
+  //     }
 
-  const addDataToList = async (): Promise<boolean> => {
-    const requestDigest = await getRequestDigest(); // Fetch digest dynamically
-    let allDataAddedSuccessfully = true; // Flag to track overall success
+  //     // Add columns to the list
+  //     for (let i = 0; i < tableHeaders.length; i++) {
+  //       const fieldType = columnTypes[i];
+  //       let metadataType = "SP.Field"; // Default metadata type
+  //       let fieldTypeKind = 2; // Default to Single Line of Text
+  //       let additionalProperties = {}; // Default additional properties
 
-    const parseCurrency = (value: string | number): number => {
-      if (typeof value === "string") {
-        return Number(value.replace(/[^0-9.]/g, "")); // Remove '$' and other non-numeric characters
-      }
-      return Number(value);
-    };
+  //       switch (fieldType) {
+  //         case "DateTime":
+  //           fieldTypeKind = 4; // DateTime Field
+  //           metadataType = "SP.FieldDateTime"; // Use the correct type for DateTime
+  //           additionalProperties = {
+  //             DisplayFormat: 0, // 0 for DateOnly
+  //           };
+  //           break;
+  //         case "Number":
+  //           fieldTypeKind = 9; // Number Field
+  //           break;
+  //         case "Currency":
+  //           fieldTypeKind = 10; // Currency Field
+  //           break;
+  //         case "Single line of text":
+  //         default:
+  //           fieldTypeKind = 2; // Single Line of Text
+  //       }
+  //       await fetch(
+  //         `${siteUrl}/_api/web/lists/getbytitle('${listName}')/fields`,
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json;odata=verbose",
+  //             "X-RequestDigest": requestDigest,
+  //           },
+  //           body: JSON.stringify({
+  //             __metadata: { type: metadataType },
+  //             Title: tableHeaders[i],
+  //             FieldTypeKind: fieldTypeKind,
+  //             ...additionalProperties,
+  //           }),
+  //         }
+  //       );
 
-    // Iterate over each row of data
-    for (const row of tableData) {
-      const itemPayload: Record<string, any> = {};
+  //       // Add the column to the default view
+  //       await fetch(
+  //         `${siteUrl}/_api/web/lists/getbytitle('${listName}')/defaultview/viewfields/addviewfield('${tableHeaders[i]}')`,
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json;odata=verbose",
+  //             "X-RequestDigest": requestDigest,
+  //           },
+  //         }
+  //       );
+  //     }
 
-      // Map headers and cell values to payload
-      tableHeaders.forEach((header, index) => {
-        const internalColumnName = header
-          .replace(/\s+/g, "_x0020_") // Replace spaces with _x0020_
-          .replace(/\//g, "_x002f_"); // Replace slashes with _x002f_
-        const cellValue = row[index]; // Get the cell value
-        if (columnTypes[index] === "Currency") {
-          const numericValue = parseCurrency(cellValue);
-          itemPayload[internalColumnName] = numericValue;
-        } else {
-          itemPayload[internalColumnName] = cellValue; // Map each header to its corresponding cell value
-        }
-      });
+  //     //alert("List created successfully!");
+  //     return true; // Return true to indicate success
+  //   } catch (error) {
+  //     console.error(error);
+  //     //alert("Error creating the SharePoint list or adding data.");
+  //     return false; // Return false if an error occurred
+  //   }
+  // };
 
-      // Try to add data to the list
-      try {
-        const response = await fetch(
-          `${siteUrl}/_api/web/lists/getbytitle('${listName}')/items`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json;odata=verbose",
-              "X-RequestDigest": requestDigest,
-            },
-            body: JSON.stringify({
-              __metadata: { type: "SP.ListItem" },
-              ...itemPayload,
-            }),
-          }
-        );
+  // const createDocumentLibrary = async (): Promise<boolean> => {
+  //   try {
+  //     const requestDigest = await getRequestDigest(); // Fetch digest dynamically
 
-        if (!response.ok) {
-          throw new Error(`Error adding item to list: ${response.statusText}`);
-        }
-      } catch (error) {
-        console.error("Failed to add data to list:", error);
-        allDataAddedSuccessfully = false; // Mark failure
-      }
-    }
+  //     const libraryPayload = {
+  //       __metadata: { type: "SP.List" },
+  //       Title: `${listName}_Documents`,
+  //       BaseTemplate: 101, // Document Library
+  //     };
 
-    // Return true if all items were added successfully, otherwise false
-    return allDataAddedSuccessfully;
-  };
+  //     const response = await fetch(`${siteUrl}/_api/web/lists`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json;odata=verbose",
+  //         "X-RequestDigest": requestDigest,
+  //       },
+  //       body: JSON.stringify(libraryPayload),
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error(`Error creating library: ${response.statusText}`);
+  //     }
+
+  //     //alert("Document library created successfully!");
+  //     return true;
+  //   } catch (error) {
+  //     console.error(error);
+  //     //alert("Error creating the document library.");
+  //     return false;
+  //   }
+  // };
+
+  // const addDataToList = async (): Promise<boolean> => {
+  //   const requestDigest = await getRequestDigest(); // Fetch digest dynamically
+  //   let allDataAddedSuccessfully = true; // Flag to track overall success
+
+  //   const parseCurrency = (value: string | number): number => {
+  //     if (typeof value === "string") {
+  //       return Number(value.replace(/[^0-9.]/g, "")); // Remove '$' and other non-numeric characters
+  //     }
+  //     return Number(value);
+  //   };
+
+  //   // Iterate over each row of data
+  //   for (const row of tableData) {
+  //     const itemPayload: Record<string, any> = {};
+
+  //     // Map headers and cell values to payload
+  //     tableHeaders.forEach((header, index) => {
+  //       const internalColumnName = header
+  //         .replace(/\s+/g, "_x0020_") // Replace spaces with _x0020_
+  //         .replace(/\//g, "_x002f_"); // Replace slashes with _x002f_
+  //       const cellValue = row[index]; // Get the cell value
+  //       if (columnTypes[index] === "Currency") {
+  //         const numericValue = parseCurrency(cellValue);
+  //         itemPayload[internalColumnName] = numericValue;
+  //       } else {
+  //         itemPayload[internalColumnName] = cellValue; // Map each header to its corresponding cell value
+  //       }
+  //     });
+
+  //     // Try to add data to the list
+  //     try {
+  //       const response = await fetch(
+  //         `${siteUrl}/_api/web/lists/getbytitle('${listName}')/items`,
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json;odata=verbose",
+  //             "X-RequestDigest": requestDigest,
+  //           },
+  //           body: JSON.stringify({
+  //             __metadata: { type: "SP.ListItem" },
+  //             ...itemPayload,
+  //           }),
+  //         }
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error(`Error adding item to list: ${response.statusText}`);
+  //       }
+  //     } catch (error) {
+  //       console.error("Failed to add data to list:", error);
+  //       allDataAddedSuccessfully = false; // Mark failure
+  //     }
+  //   }
+
+  //   // Return true if all items were added successfully, otherwise false
+  //   return allDataAddedSuccessfully;
+  // };
   const navigate = useNavigate(); // React Router hook for navigation
 
   const fileInputRef = React.useRef<HTMLInputElement | null>(null);
